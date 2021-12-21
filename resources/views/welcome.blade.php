b38172709e246f2976047074f478c8efa80bad59
pablocontreras <pablo_contreras_1997@outlook.com>
2021-12-21 15:12:57 -0600

    Completed

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,16 +1,26 @@
-<!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+<html lang="en" >
     <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-
-        <title>Laravel</title>
-
-        <!-- Fonts -->
-        <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap" rel="stylesheet">
-
+        <title>RAKITAPLIKASI.COM - Convert image to webp</title>
+        <link rel="stylesheet" href="https://rakitaplikasi.com/css/normalize.css">
+        <link rel="stylesheet" href="https://rakitaplikasi.com/css/webp-converter.css">
     </head>
     <body>
-        
+
+        <!-- partial:index.partial.html -->
+        <div class="layout">
+            <h1>RAKITAPLIKASI.COM - Convert image to webp format</h1>
+            <div>
+                <input type="file" multiple accept="image/*">
+            </div>
+            <br>
+            <div id="previews"></div>
+            <div class="dropTarget"></div>
+        </div>
+
+
+        <script  src="https://rakitaplikasi.com/js/webp-converter.js"></script>
+        <script type="text/javascript">
+            document.getElementsByTagName("h1")[0].style.fontSize = "30px";
+        </script>
     </body>
 </html>
